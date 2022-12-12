b615db5865a3ba7dd2e80686052267441af8fae0
Rafał Łukasik <Rafal.Lukasik@assecobs.pl>
2022-12-12 14:28:00 +0100

    test2

---
+++ index.js
@@ -0,0 +1,81 @@
+const path = require('path'); //dostęp do ścieżki aplikacji
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+
+const WebSocket = require('ws');
+
+
+
+const bodyParser = require('body-parser');
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    server = require('http').createServer(app);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const wss = new WebSocket.Server({
+    server
+});
+
+var webSockets = {};
+
+
+app.get('*', async function (req, res, next) {
+    return next();
+});
+
+wss.on('connection', function (ws, req) {
+    console.log(req.url);
+    var type = req.url.substr(1, 3);
+    webSockets[type] = ws;
+    console.log("Connected: " + type);
+
+    ws.on('message', function (message) {
+        let parsedMessage;
+        try {
+            parsedMessage = JSON.parse(message);
+        } catch (err) {
+            parsedMessage = message;
+        }
+        console.log(parsedMessage);
+
+        switch (parsedMessage.type) {
+            case "toService":
+                console.log(parsedMessage);
+            webSockets['dev'].send(JSON.stringify(parsedMessage));
+                break;
+            case "fromDevice":
+                console.log(parsedMessage);
+                webSockets['loc'].send(JSON.stringify(parsedMessage));
+                    break;
+            default:
+                break;
+        }
+
+        /*if (type == 'src' && webSockets['des']) {
+            webSockets['des'].send(JSON.stringify({
+                callbackId: parsedMessage.callbackId,
+                response: parsedMessage.response,
+                errorStatus: parsedMessage.errorStatus
+            }));
+        } else if (type == 'des' && webSockets['src']) {
+            webSockets['src'].send(JSON.stringify(parsedMessage));
+        }*/
+        return false;
+    })
+
+    ws.on('close', function (ev) {})
+
+    return false;
+})
+
+server.listen(port, () => {
+    console.log("To open up a presentation, open your browser and type in this address: http://localhost:" + port);
+});
